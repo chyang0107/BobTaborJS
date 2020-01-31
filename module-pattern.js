8acb6622d787dbeb3adbd98321c475150cd1b29d
changheng yang <hank@changhengs-MacBook-Pro.local>
2020-01-31 00:06:03 -0600

    learning Module

--- module-pattern.js
+++ module-pattern.js
@@ -8,19 +8,35 @@ var counter = (function () {
     } 
     //return an object
     return {
-        value: count,
-        
+        // value: count,
+        get: function() {
+            return count;
+        },
+        set: function (value) {
+            count = value
+        },
+    
         increment: function () {
             count += 1;
             print('After increment: ')
         },
 
         reset: function ()  {
-            
+            print('Before reset: ');
+            count = 0;
+            print('After rest: ')
         }
-
     }
-
 }
-
 )();
+// count is not the property of the object by the return
+// console.log(counter.count)
+counter.increment();
+counter.increment();
+counter.increment();
+
+// Accidently created a closure
+// console.log(counter.value)
+counter.set(7);
+console.log(counter.get());
+counter.reset();
