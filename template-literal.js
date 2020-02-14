ca3c4028959dba110bfc7ada1cd2c780d019a2e4
Changheng Yang <chayang@south-murray-south-private-dip136.nat.okstate.edu>
2020-02-14 16:52:46 -0600

    string literal practice

--- template-literal.js
+++ template-literal.js
@@ -1,2 +1,15 @@
-let name = 'bob';
-console.log(`hi ${name}`);
+// let name = 'bob';
+// console.log(`hi ${name}`);
+
+// let sentence = `This is why I like the 
+// Javascri and ASP.NET
+// `
+
+// console.log(sentence)
+
+function getReasonCount() {
+    return 2;
+}
+
+let interpolation = `Give me ${(getReasonCount()==1)?'one good reason':'a few reasons'} to try this.`
+console.log(interpolation);
