241450649a8b6e9dbba77fbe4be83f8a1ea2a40e
changheng yang <hank@changhengs-MacBook-Pro.local>
2020-02-15 19:33:52 -0600

    test native

--- native.js
+++ native.js
@@ -1,1 +1,12 @@
-let pattern = /xyz/;
+// let pattern = /xyz/;
+// let value = 'This is xyz test.';
+// console.log(value.replace(pattern, 'just'));
+
+let myString = new String('howdy')
+console.log(typeof myString);
+console.log(myString.toString());
+
+console.log(typeof myString);
+
+
+
