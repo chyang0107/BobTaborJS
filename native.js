0240a2500ed7d2156da3ace649dd68cc07c929ea
Changheng Yang <chayang@south-murray-south-private-dip136.nat.okstate.edu>
2020-02-21 10:15:25 -0600

    native comiler box and unbox

--- native.js
+++ native.js
@@ -2,14 +2,27 @@
 // let value = 'This is xyz test.';
 // console.log(value.replace(pattern, 'just'));
 
-let myString = new String('howdy')
-console.log(typeof myString);
-console.log(myString.toString());
+// let myString = new String('howdy')
+// console.log(typeof myString);
+// console.log(myString.toString());
 
-console.log(typeof myString);
+// console.log(typeof myString);
 
 //We can use JavaScript’s built-in constructor functions 
 //(just like the ones we’ve created) to create objects:
+
+
+// It is all about Javascript Boxing and unBoxing 
+
+// let myPrimitive = 'THIS IS CRAZY';
+// console.log(typeof myPrimitive);
+// myPrimitive = myPrimitive.toLowerCase();
+// console.log(typeof myPrimitive);
+
+let myNumber = new Number(7);
+console.log(typeof myNumber);
+let myPrimitie = myNumber.valueOf();
+console.log(typeof myPrimitie);
 
 
 
