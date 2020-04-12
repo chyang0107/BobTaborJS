6c3d5e888c444b07ac239a7f9112d4c9b3a18ebe
Changheng Yang <chayang@MUR361-CYang.local>
2020-04-12 18:52:27 -0500

    constructor 2

--- constructor.js
+++ constructor.js
@@ -1,11 +1,23 @@
 // Construction Function Calls with the new keyword
 // Date: 4/12
 
-function Car(make, model, year) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
+
+// this initiate the new property
+// the property get the value from the parameter 
+
+// function Car(make, model, year) {
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+// }
+// new --> empty object
+
+// let myCar = new Car('bmw','745i', 2010)
+// console.log(typeof myCar);
+
+function MyFunction() {
+    console.log('I am a simple function');
 }
 
-let myCar = new Car('bmw','745i', 2010)
-console.log(typeof myCar);
+let myFunction = new MyFunction();
+console.log(typeof myFunction);
