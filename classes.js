d9955070947522064316b61beef3eec66e4dded0
Changheng Yang <chayang@MUR361-CYang.local>
2020-05-05 18:23:29 -0500

    25 Javascript Classes finish

--- classes.js
+++ classes.js
@@ -2,7 +2,7 @@
 
 class Car {
     constructor(make, model, year){
-        this.make.make;
+        this.make = make;
         this.model = model;
         this.year = year;
     }
@@ -15,6 +15,16 @@ class Car {
 let myCar = new Car('bmw', '745li', 2010)
 myCar.print();
 
+class SportCar extends Car {
+    revEnginer(){
+        console.log('Vrrom goes the ' + this.model);
+    }
+}
+
+let mySportsCar = new SportCar('dodge', 'viper', 2011);
+mySportsCar.print();
+mySportsCar.revEnginer();
+// myCar.revEnginer();
 
 //expression
 // let car = class {
