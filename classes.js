721c4b4fd570ca89c0f6fc0fc3efff6744891ecb
Changheng Yang <chayang@MUR361-CYang.local>
2020-05-05 18:12:55 -0500

    class tutorial

---
+++ classes.js
@@ -0,0 +1,22 @@
+//declaration
+
+class Car {
+    constructor(make, model, year){
+        this.make.make;
+        this.model = model;
+        this.year = year;
+    }
+    print(){
+        console.log(`${this.make} ${this.model} ${this.year}`);
+    }
+
+}
+
+let myCar = new Car('bmw', '745li', 2010)
+myCar.print();
+
+
+//expression
+// let car = class {
+
+// }
