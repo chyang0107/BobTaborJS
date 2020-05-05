540d572b93ac34a2914f2436541c48917102f4a3
Changheng Yang <chayang@MUR361-CYang.local>
2020-05-05 18:06:34 -0500

    class prototype finish

--- prototype.js
+++ prototype.js
@@ -1,1 +1,27 @@
 // Date: 05/05/2020
+// Relationship between new car and original car
+let originator = {
+    make: 'bmw',
+    model: '745i',
+    year: 2010
+}
+
+let newCar = Object.create(originator);
+console.log(newCar.make);
+
+newCar.make = 'audi'
+console.log(newCar.make);
+console.log(newCar.whatever);
+
+console.log(Object.getPrototypeOf(newCar));
+
+
+originator.doors = 4;
+console.log(newCar.doors);
+
+console.log(originator.hasOwnProperty('doors'));
+console.log(newCar.hasOwnProperty('doors'));
+
+
+
+
