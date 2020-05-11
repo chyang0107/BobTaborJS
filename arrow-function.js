f2c726d8433cac5b1d7fd8e62d157ab64a8a83b0
Changheng Yang <chayang@MUR361-CYang.local>
2020-05-11 13:29:25 -0500

    map function

--- arrow-function.js
+++ arrow-function.js
@@ -1,4 +1,19 @@
 // Date: 5/11/2020
 
-let hi = () => {console.log('howdy');}
-hi();
+// let hi = (argument) => {console.log('howdy');}
+// hi();
+
+// let hi = (name) => {console.log(`howdy ${name}`)};
+// hi('bob');
+
+// Return 
+
+// let add = (a, b) => {return a+b };
+// console.log(add(7,3));
+
+// The map() method in JavaScript creates an array 
+// by calling a specific function 
+// on each element present in the parent array.
+
+let names = ['david', 'eddie', 'alex', 'michael'];
+names.map( (name) =>   )
