99593fa2ff9f139e7f75028305e66ac2eee9c69b
Changheng Yang <chayang@MUR361-CYang.local>
2020-05-18 03:32:16 -0500

    String Method practice

--- date.js
+++ date.js
@@ -11,4 +11,36 @@ let bob = new Date('December 7, 1969 07:01:23');
 
 //get parts
 // 
-console.log(bob.getTime());
+// console.log(bob.getTime());
+
+let first = 'Knowledge is power but enthusiasm pulls the switch'
+let second = 'Do or Do not.  There is no try.';
+
+let third = '4,8,15,16,23,43';
+
+// console.log('bob love you'.toUpperCase());
+
+// let mySplit  = third.split(',');
+// console.log(mySplit);
+
+// let mySlice = first.slice(13, 18)
+// console.log(mySlice);
+
+let mySubstr = first.substr(13, 5);//(start, length)
+console.log(mySubstr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
