cef9ae29dd18c0d5c01d8172bd1fe58654bff665
Changheng Yang <chayang@MUR361-CYang.local>
2020-05-23 18:23:43 -0500

    array method practice

---
+++ arrayMethods.js
@@ -0,0 +1,14 @@
+let names = ['david','eddie', 'alex', 'michael'];
+let others = ['sammy', 'gary', 'wolfgang', 'mark'];
+
+let lost = [4,8,15,16,23,42];
+let fibonacci = [1,1,2,3,5,8,13,21,34,55];
+
+// var combine = lost.concat(others);
+// console.log(combine);
+
+// console.log(fibonacci.join('~'));
+
+//push, pop
+console.log(lost.shift());
+console.log(lost);
