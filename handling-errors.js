ddf1c2a6f2de2c97566cc9593a260d7b535cd50a
Changheng Yang <chayang@MUR361-CYang.local>
2020-05-31 23:26:57 -0500

    error handling part 2

--- handling-errors.js
+++ handling-errors.js
@@ -7,4 +7,19 @@ function beforeTryCatch() {
     console.log('If the previous line of code throws an exception you \'ll never see this');
 }
 
-beforeTryCatch();
+// beforeTryCatch();
+
+function afterTryCatch() {
+    try {
+        let obj = undefined;
+        console.log(obj.b);
+        console.log('If the previous line of code throws an exception you \'ll never see this');
+    } catch (error) {
+        console.log('I caught an exception: '+ error.message);
+    } finally{
+        console.log('This will happen no matter what.');
+    }
+    console.log('My application is still runnung!');
+}
+
+afterTryCatch();
