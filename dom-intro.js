b33f2e07a2f8711b6e86ebf392ea4959794faca5
Changheng Yang <chayang@MUR361-CYang.local>
2020-06-08 00:36:03 -0500

    dom practice again

---
+++ dom-intro.js
@@ -0,0 +1,12 @@
+(function () {
+    function clickHandler(message){
+        console.log('hi......' + message)
+    }
+
+    // Get a reference to my Button
+    let myButton = document.getElementById('myButton');
+    myButton.addEventListener('click', clickHandler('hi frim life'));
+
+
+
+})();
